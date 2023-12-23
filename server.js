5943de986dfd4e50853de5cd01fcd943ff17fea6
Roksan <glehdtm@hanmail.net>
2023-12-24 02:06:58 +0900

    ㅎㅎ

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { express, http } = require('./Lib/http')
 const enums = require('./Scripts/enums')
 
 // 웹서버 올리기
-http.listen(config.PORT_WEB, () => log.add_Color('ffff00', `WebServer start :: ${config.PORT_WEB}`))
+http.listen(config.PORT_WEB, () => log.add_Color('ffff00', `1224: WebServer start :: ${config.PORT_WEB}`))
 
 express.use('/roksan', require('./Scripts/ServerMain/web_main'))
 
